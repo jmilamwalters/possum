ea82f846dc03ced91a6fe6895d7aaa1a5aa456fd
jwietelmann <jwietelmann@gmail.com>
2016-10-14 13:17:01 -0400

    improve the Select component

--- src/Select.example.js
+++ src/Select.example.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react'
 import Select from './Select'
-import Option from './Option'
 
-export class Example extends Component {
+export class PickOne extends Component {
   render() {
     return (
       <Select name="airport" label="Airport">
-        <Option value="MSY">Louis Armstrong</Option>
-        <Option value="JFK">John F. Kennedy</Option>
+        <Select.Option value="MSY">Louis Armstrong</Select.Option>
+        <Select.Option value="JFK">John F. Kennedy</Select.Option>
+      </Select>
+    )
+  }
+}
+
+export class PickMultiple extends Component {
+  render() {
+    return (
+      <Select multiple name="airports[]" label="Airports">
+        <Select.Option value="MSY">Louis Armstrong</Select.Option>
+        <Select.Option value="JFK">John F. Kennedy</Select.Option>
+      </Select>
+    )
+  }
+}
+
+export class HasError extends Component {
+  render() {
+    return (
+      <Select name="airport" label="Airport" error="Oh no, a bad thing!">
+        <Select.Option value="MSY">Louis Armstrong</Select.Option>
       </Select>
     )
   }
--- src/Select.js
+++ src/Select.js
@@ -1,10 +1,45 @@
-import React from 'react'
-import Input from './Input'
+import React, {Component} from 'react'
+import classNames from 'classnames'
+import _ from 'underscore'
 
-export default class Select extends React.Component {
+class Select extends Component {
+
+  static defaultProps = {
+    error: null,
+    className: '',
+  };
+
+  static Option = (props) => <option {...props}>{props.children}</option>;
+
+  get error() {
+    if(!this.props.error) {
+      return null
+    }
+    return (
+      <span className="form-error is-visible">{this.props.error}</span>
+    )
+  }
+
+  get labelClassName() {
+    return classNames(this.props.className, {'is-invalid-label': !!this.props.error})
+  }
+
+  get selectClassName() {
+    return classNames({'is-invalid-input': !!this.props.error})
+  }
+
   render() {
-    return <Input {...this.props} dom="select">
-      {this.props.children}
-    </Input>
+    const selectProps = _.omit(this.props, Object.keys(this.constructor.defaultProps))
+    return (
+      <label className={this.labelClassName}>
+        {this.props.label}
+        <select {...selectProps} className={this.selectClassName}>
+          {this.props.children}
+        </select>
+        {this.error}
+      </label>
+    )
   }
 }
+
+export default Select
