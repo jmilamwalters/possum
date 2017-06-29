3c77545856d1402ed98bbe344db8b5985bd6d45c
CJ Horton <thecjhorton@gmail.com>
2017-06-29 17:27:02 -0500

    example file for CheckableFieldset

---
+++ src/CheckableFieldset.example.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export class Example extends Component {
+  render() {
+    return(
+      <div>
+        <p>See <a href="/Checkbox.html#top">Checkbox</a> for example usage.</p>  
+      </div>
+    )
+  }
+}
