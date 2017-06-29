583419f2bd919a15835ed0fe4e0ce09d40b9e1e1
CJ Horton <thecjhorton@gmail.com>
2017-06-29 13:49:13 -0500

    add example file for ChartBuilder

---
+++ src/ChartBuilder.example.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+export class Charts extends Component {
+  render() {
+      return (
+        <div>
+          <p>
+            See the <a href="http://www.chartjs.org/docs/latest/" target="_blank">Chart.JS</a> docs for usage instructions.  
+          </p>
+        </div>
+        
+      )
+    
+  }
+}
