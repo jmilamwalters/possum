05fcae9d435382e6bd42c92b210df5d40c9710f1
CJ Horton <thecjhorton@gmail.com>
2017-06-29 13:48:50 -0500

    add tests for CardLayout

--- src/CardLayout.test.js
+++ src/CardLayout.test.js
@@ -9,4 +9,16 @@ describe('CardLayout', () => {
       </CardLayout>
     )
   })
+
+  it('should add className to child', () => {
+    const inherentClassName = 'rev-CardLayout'
+    const testClassName = '__TEST__'
+
+    const childClassName = shallow(<CardLayout className={testClassName} />)
+      .first()
+      .prop('className')
+
+    expect(childClassName).to.contain(inherentClassName)
+    expect(childClassName).to.contain(testClassName)
+  })
 })
