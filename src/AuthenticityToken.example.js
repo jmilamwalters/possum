255abe95364b65db47bffade7d7004a7b25f3066
CJ Horton <thecjhorton@gmail.com>
2017-06-29 13:47:48 -0500

    example for AuthenticityToken (points to Form)

---
+++ src/AuthenticityToken.example.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export class Token extends Component {
+  render() {
+    console.log("doing the thing")
+    return (
+      <div>
+        <p>See <a href="/Form.html#top">Form</a> for usage.</p>
+      </div>
+    )
+  }
+}
