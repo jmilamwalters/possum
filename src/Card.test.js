33075cd693e96b4c04680ad30e6c3946540079f9
CJ Horton <thecjhorton@gmail.com>
2017-06-29 13:48:41 -0500

    add tests for Card

--- src/Card.test.js
+++ src/Card.test.js
@@ -12,4 +12,16 @@ describe('Card', () => {
       </Card>
     )
   })
+
+  it('should add className to child', () => {
+    const inherentClassName = 'rev-Card'
+    const testClassName = '__TEST__'
+
+    const childClassName = shallow(<Card className={testClassName} />)
+      .first()
+      .prop('className')
+
+    expect(childClassName).to.contain(inherentClassName)
+    expect(childClassName).to.contain(testClassName)
+  })
 })
