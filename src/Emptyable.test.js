52acee9c69ab48f52c1cfb06fcef9c34aa5c8ee9
CJ Horton <thecjhorton@gmail.com>
2017-07-03 12:33:52 -0500

    expand tests for Emptyable

--- src/Emptyable.test.js
+++ src/Emptyable.test.js
@@ -6,4 +6,24 @@ describe('Emptyable', () => {
       <Emptyable componentClass="ul" emptyState={<strong>No items</strong>} />
     )
   })
+
+  it('should render children when present', () => {
+    const hasChildren = shallow(
+      <Emptyable componentClass="ul" 
+        emptyState={<strong>No items</strong>}>
+          <li>I am not empty.</li>
+      </Emptyable>
+    )
+
+    expect(hasChildren.text()).not.to.contain("No items")
+  })
+
+  it('should render empty state with no children', () => {
+    const noChildren = shallow(
+      <Emptyable 
+        emptyState={<strong>No items</strong>}></Emptyable>
+    )
+
+    expect(noChildren.text()).to.contain("No items")
+  })
 })
