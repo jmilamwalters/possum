747463239be537cfd626426eba341b83d6443138
CJ Horton <thecjhorton@gmail.com>
2017-07-03 18:30:48 -0500

    MediaObject tests

--- src/MediaObject.test.js
+++ src/MediaObject.test.js
@@ -4,10 +4,34 @@ describe('MediaObject', () => {
   it('should render without throwing', () => {
     shallow(<MediaObject />)
   })
+
+  it('should add className to child', () => {
+    const inherentClassName = 'media-object'
+    const testClassName = '__TEST__'
+
+    const childClassName = shallow(<MediaObject className={testClassName} />)
+      .first()
+      .prop('className')
+
+    expect(childClassName).to.contain(inherentClassName)
+    expect(childClassName).to.contain(testClassName)
+  })  
 })
 
 describe('MediaObjectSection', () => {
   it('should render without throwing', () => {
     shallow(<MediaObject.Section />)
+  })
+
+  it('should add className to child', () => {
+    const inherentClassName = 'media-object-section'
+    const testClassName = '__TEST__'
+
+    const childClassName = shallow(<MediaObject.Section className={testClassName} />)
+      .first()
+      .prop('className')
+
+    expect(childClassName).to.contain(inherentClassName)
+    expect(childClassName).to.contain(testClassName)
   })
 })
