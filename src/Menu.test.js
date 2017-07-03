5b31e15c79549ca44cefaf1401eb19335c909604
CJ Horton <thecjhorton@gmail.com>
2017-07-03 18:30:55 -0500

    Menu tests

--- src/Menu.test.js
+++ src/Menu.test.js
@@ -14,4 +14,29 @@ describe('Menu', () => {
       </Menu>
     )
   })
+
+  it('should add className to child', () => {
+    const inherentClassName = 'menu'
+    const testClassName = '__TEST__'
+
+    const childClassName = shallow(<Menu className={testClassName} />)
+      .first()
+      .prop('className')
+
+    expect(childClassName).to.contain(inherentClassName)
+    expect(childClassName).to.contain(testClassName)
+  })
+
+  it('should render correct elements', () => {
+    const elementCheck = shallow(
+      <Menu vertical>
+        <Menu.Item>
+          <a href="#">One</a>
+        </Menu.Item>
+      </Menu>
+    )
+    
+    expect(elementCheck.find('ul')).to.be.ok
+    expect(elementCheck.find('li')).to.be.ok
+  })
 })
