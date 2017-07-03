aa03346554c7a362f8f6c5bace80abed8226bebe
CJ Horton <thecjhorton@gmail.com>
2017-07-03 18:01:22 -0500

    clarify examples for InputErrors, InputHelpText, InputLabel

---
+++ src/InputLabel.example.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+
+export class InputLabelExample extends Component {
+  render() {
+    return (
+      <div>
+        See other input types for usage: 
+        <ul>
+          <li>
+            <a href="/CheckableFieldset.html#top">CheckableFieldset</a> 
+          </li>
+          <li>
+            <a href="/Checkbox.html#top">Checkbox</a>
+          </li>
+          <li>
+            <a href="/FileInput.html#top">FileInput</a>
+          </li>
+          <li>
+            <a href="/Input.html#top">Input</a>
+          </li>
+          <li>
+            <a href="/InputGroup.html#top">InputGroup</a>
+          </li>
+          <li>
+            <a href="/Radio.html#top">Radio</a>
+          </li>
+          <li>
+            <a href="/Select.html#top">Select</a>
+          </li>
+          <li>
+            <a href="/Textarea.html#top">Textarea</a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
