3a6dd61e2298e1b172eae4edc310eacd55f1ff4f
CJ Horton <thecjhorton@gmail.com>
2017-07-03 19:10:19 -0500

    update Option example

---
+++ src/Option.example.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+export class OptionExample extends Component {
+  render() {
+    return (
+      <p>Don't use this. Use the options property on the <a href="/Select.html#top">Select</a> component instead.</p>
+    )
+  }
+}
