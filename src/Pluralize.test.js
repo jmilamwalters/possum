f964ca0c6ffa4c2c158dbcfc28587706921c31b7
CJ Horton <thecjhorton@gmail.com>
2017-07-03 20:00:58 -0500

    Pluralize tests

--- src/Pluralize.test.js
+++ src/Pluralize.test.js
@@ -6,4 +6,15 @@ describe('Pluralize', () => {
       <Pluralize one="mouse" count={1}/>
     )
   })
+
+  it('should pluralize correctly', () => {
+    const wrapper = shallow(<Pluralize one="mouse" count={1}/>)
+    expect(wrapper.text()).to.equal("1 mouse")
+
+    wrapper.setProps({ one: "spoon", count: 3 })
+    expect(wrapper.text()).to.equal("3 spoons")
+
+    wrapper.setProps({ more: "mice" })
+    expect(wrapper.text()).to.equal("3 mice")
+  })
 })
