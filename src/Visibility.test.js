b2356cb0d37ec379dbdc23a61dc3f843d5bd8f70
CJ Horton <thecjhorton@gmail.com>
2017-07-05 11:20:56 -0500

    Visibility tests

--- src/Visibility.test.js
+++ src/Visibility.test.js
@@ -6,4 +6,16 @@ describe('Visibility', () => {
       <Visibility showForLarge><span>showForLarge</span></Visibility>
     )
   })
+
+  it('should add className to child', () => {
+    const inherentClassName = 'show-for-small'
+    const testClassName = '__TEST__'
+
+    const childClassName = shallow(
+      <Visibility showForSmall className={testClassName}><span>showForSmall</span></Visibility>
+    ).first().prop('className')
+
+    expect(childClassName).to.contain(inherentClassName)
+    expect(childClassName).to.contain(testClassName)
+  })
 })
